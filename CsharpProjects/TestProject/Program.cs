b0f071dec5e972546914fff1c7d38a71b527ff7f
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-30 15:05:00 +0200

    If-else-else-if

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -105,43 +105,73 @@
 // }
 
 // Use if, else, and else if statements to give a prize instead of a win-lose message - reset code to re-run hard-test
-Random dice = new Random();
+// Random dice = new Random();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
 
-int total = roll1 + roll2 + roll3;
+// int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     if ((roll1 == roll2) && (roll2 == roll3))
+//     {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6;
+//     }
+//     else
+//     {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+// }
+
+// if (total >= 16)
+// {
+//     Console.WriteLine("You win a new car!");
+// }
+// else if (total >= 10)
+// {
+//     Console.WriteLine("You win a new laptop!");
+// }
+// else if (total == 7)
+// {
+//     Console.WriteLine("You win a trip for two!");
+// }
+// else
+// {
+//     Console.WriteLine("You win a kitten!");
+// }
+
+// Complete a challenge activity to apply business rules Exercise
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here - (solution below)
+if (daysUntilExpiration == 0)
 {
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
 
-if (total >= 16)
+if (discountPercentage > 0)
 {
-    Console.WriteLine("You win a new car!");
-}
-else if (total >= 10)
-{
-    Console.WriteLine("You win a new laptop!");
-}
-else if (total == 7)
-{
-    Console.WriteLine("You win a trip for two!");
-}
-else
-{
-    Console.WriteLine("You win a kitten!");
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
