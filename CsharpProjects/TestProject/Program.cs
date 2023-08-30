8b02d80e2d65b983e4380cacfe6a630497c7710d
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-30 16:27:17 +0200

    effective code comments

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -218,12 +218,98 @@ fraudulentOrderIDs[2] = "C789";
 // Console.WriteLine($"We have {sum} items in inventory.");
 
 // nested iteration and selection statements challenge activity
-string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+// string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
-foreach (string orderID in orderIDs)
+// foreach (string orderID in orderIDs)
+// {
+//     if (orderID.StartsWith("B"))
+//     {
+//         Console.WriteLine(orderID);
+//     }
+// }
+
+// Exercise - Create effective code comments
+//#1.
+// string firstName = "Bob";
+// int widgetsPurchased = 7;
+// // Testing a change to the message.
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+// Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+
+//#2.
+/*
+string firstName = "Bob";
+int widgetsPurchased = 7;
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+*/
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+// Loop through each blank orderID
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+    // Get a random value that equates to ASCII letters A through E
+    // int prefixValue = random.Next(65, 70);
+    // Convert the random value into a char, then a string
+    // string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, pad with zeroes
+    // string suffix = random.Next(1, 1000).ToString("000");
+    // Combine the prefix and suffix together, then assign to current OrderID
+    // orderIDs[i] = prefix + suffix;
+// }
+// Print out each orderID
+// foreach (var orderID in orderIDs)
+// {
+    // Console.WriteLine(orderID);
+// }
+
+// #2.2
+// /*
+//   The following code creates five random OrderIDs
+//   to test the fraud detection process.  OrderIDs 
+//   consist of a letter from A to E, and a three
+//   digit number. Ex. A123.
+// */
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+//     int prefixValue = random.Next(65, 70);
+//     string prefix = Convert.ToChar(prefixValue).ToString();
+//     string suffix = random.Next(1, 1000).ToString("000");
+
+//     orderIDs[i] = prefix + suffix;
+// }
+
+// foreach (var orderID in orderIDs)
+// {
+//     Console.WriteLine(orderID);
+// }
+
+// #3.
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
 {
-    if (orderID.StartsWith("B"))
+    if (letter == 'o')
     {
-        Console.WriteLine(orderID);
+        letterCount++;
     }
 }
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
