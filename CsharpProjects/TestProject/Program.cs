09df9fefbee47a4a656ddd13b44722743448ed42
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-30 14:29:55 +0200

    Nested if else exercise

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -26,10 +26,40 @@ Console.WriteLine("Hello C#!");
 // dice.Next(0);
 
 // Math class methods challenge
-int firstValue = 500;
-int secondValue = 600;
-int largerValue;
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue;
 
-largerValue = Math.Max(firstValue, secondValue);
+// largerValue = Math.Max(firstValue, secondValue);
 
-Console.WriteLine(largerValue);
+// Console.WriteLine(largerValue);
+
+// Create decision logic with if statements
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// Add another if statement to implement the doubles bonus
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    // Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    Console.WriteLine("You rolled triples! +6 bonus to total!"); // modified new Writeline
+    total += 2;
+}
+
+// Add an if statement to display different messages based on the value of the total variable
+if (total >= 15) // changed second if
+{
+    Console.WriteLine("You win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you lose.");
+}
