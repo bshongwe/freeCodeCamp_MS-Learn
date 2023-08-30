4bf69cd86ecca4910a15c34af88b456acbb28d25
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-30 14:41:35 +0200

    Hard testing for nested decision logic with if, else if, and else

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello C#!");
+// Console.WriteLine("Hello C#!");
 
 // Random dice = new Random();
 // int roll = dice.Next(1, 7);
@@ -35,6 +35,76 @@ Console.WriteLine("Hello C#!");
 // Console.WriteLine(largerValue);
 
 // Create decision logic with if statements
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// Add another if statement to implement the doubles bonus
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     // Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//     Console.WriteLine("You rolled triples! +6 bonus to total!"); // modified new Writeline
+//     total += 2;
+// }
+
+// Add an if statement to display different messages based on the value of the total variable
+// if (total >= 15) // changed second if
+// {
+//     Console.WriteLine("You win!");
+// }
+
+// if (total < 15)
+// {
+//     Console.WriteLine("Sorry, you lose.");
+// }
+
+// Exercise - Create nested decision logic with if, else if, and else
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// Hardcode Test
+// roll1 = 6;
+// roll2 = 6;
+// roll3 = 5;
+// roll3 = 6; // Hard test step 2
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     if ((roll1 == roll2) && (roll2 == roll3))
+//     {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6;
+//     }
+//     else
+//     {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+// }
+
+// if (total >= 15)
+// {
+//     Console.WriteLine("You win!");
+// }
+// else 
+// {
+//     Console.WriteLine("Sorry, you lose.");
+// }
+
+// Use if, else, and else if statements to give a prize instead of a win-lose message - reset code to re-run hard-test
 Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
@@ -45,21 +115,33 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-// Add another if statement to implement the doubles bonus
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    // Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    Console.WriteLine("You rolled triples! +6 bonus to total!"); // modified new Writeline
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
 }
 
-// Add an if statement to display different messages based on the value of the total variable
-if (total >= 15) // changed second if
+if (total >= 16)
 {
-    Console.WriteLine("You win!");
+    Console.WriteLine("You win a new car!");
 }
-
-if (total < 15)
+else if (total >= 10)
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
