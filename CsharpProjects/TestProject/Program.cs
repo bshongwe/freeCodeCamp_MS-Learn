507086acd12b10851b9b002e35d09656a4534e1a
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-30 13:25:59 +0200

    discover and implement a method call challenge

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -7,20 +7,29 @@ Console.WriteLine("Hello C#!");
 
 // Overloaded methods
 // #1.
-int number = 7;
-string text = "seven";
+// int number = 7;
+// string text = "seven";
 
-Console.WriteLine(number);
-Console.WriteLine();
-Console.WriteLine(text);
+// Console.WriteLine(number);
+// Console.WriteLine();
+// Console.WriteLine(text);
 
 // #2.
-Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
-dice.Next(0);
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+// dice.Next(0);
+
+// Math class methods challenge
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
