613f587dc38c756f32d8852065f50bbdb61028d6
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-30 15:20:18 +0200

    Array int

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -186,14 +186,33 @@ fraudulentOrderIDs[2] = "C789";
 // fraudulentOrderIDs[3] = "D000";
 */
 
-string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+// string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
 
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
-fraudulentOrderIDs[0] = "F000";
+// fraudulentOrderIDs[0] = "F000";
 
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+// Exercise - Implement the foreach statement
+// string[] names = { "Rowena", "Robin", "Bao" };
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// array of int
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
