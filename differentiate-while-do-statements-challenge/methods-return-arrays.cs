efc8077bb031c218878c3eb3ec5928b6cfe2025e
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-02 19:25:41 +0200

    Lost project

--- differentiate-while-do-statements-challenge/methods-return-arrays.cs
+++ differentiate-while-do-statements-challenge/methods-return-arrays.cs
@@ -1,3 +1,24 @@
+int target = 30;
+int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+int[,] result = TwoCoins(coins, target);
+
+if (result.Length == 0) 
+{
+    Console.WriteLine("No two coins make change");
+} 
+else 
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < result.GetLength(0); i++) 
+    {
+        if (result[i,0] == -1) 
+        {
+            break;
+        }
+        Console.WriteLine($"{result[i,0]},{result[i,1]}");
+    }
+}
+
 int[,] TwoCoins(int[] coins, int target) 
 {
     int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
@@ -6,7 +27,7 @@ int[,] TwoCoins(int[] coins, int target)
     for (int curr = 0; curr < coins.Length; curr++) 
     {
         for (int next = curr + 1; next < coins.Length; next++) 
-        {
+        {    
             if (coins[curr] + coins[next] == target) 
             {
                 result[count, 0] = curr;
