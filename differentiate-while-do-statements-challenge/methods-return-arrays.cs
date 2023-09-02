ddd6a1c3cfd40b02a190684a70a8cb98d4e19b13
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-02 19:24:06 +0200

    Lost project

---
+++ differentiate-while-do-statements-challenge/methods-return-arrays.cs
@@ -0,0 +1,23 @@
+int[,] TwoCoins(int[] coins, int target) 
+{
+    int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+    int count = 0;
+
+    for (int curr = 0; curr < coins.Length; curr++) 
+    {
+        for (int next = curr + 1; next < coins.Length; next++) 
+        {
+            if (coins[curr] + coins[next] == target) 
+            {
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
+            }
+            if (count == result.GetLength(0)) 
+            {
+                return result;
+            }
+        }
+    }
+    return (count == 0) ? new int[0,0] : result;
+}
