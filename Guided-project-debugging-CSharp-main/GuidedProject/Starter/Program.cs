b16e22efbda0726f5d7cf7f7ddf4ae29bf8f328f
bshongwe <dru.da.beat.hustle>
2023-09-25 17:40:46 +0200

    Make change edit

--- Guided-project-debugging-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-debugging-CSharp-main/GuidedProject/Starter/Program.cs
@@ -145,7 +145,7 @@ static string MakeChange(int cost, int[] cashTill, int twenties, int tens = 0, i
 
     while ((changeNeeded > 4) && (cashTill[1] > 0))
     {
-        cashTill[2]--;
+        cashTill[1]--;
         changeNeeded -= 5;
         Console.WriteLine("\t A five");
     }
